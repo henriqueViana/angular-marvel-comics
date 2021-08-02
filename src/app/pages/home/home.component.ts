c787dfb1e5ed1364042086ba68745cc81e211aa8
Henrique Viana <hsantos@lopes.com.br>
2021-08-02 11:55:36 -0300

    Create initial home page

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+import { Store, select } from '@ngrx/store'
+
+import { add } from '../../store/comics/actions' 
+import { ComicsService } from '../../services/comics/comics.service'
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  public comics$: any;
+  public typeThumb = '/portrait_xlarge.'
+
+  public comicsState$: Observable<any> | undefined
+
+  constructor(
+    private router: Router,
+    private store: Store<any>,
+    private comicsService: ComicsService
+  ) { }
+
+  ngOnInit(): void {
+    this.comicsState$ = this.store.pipe(
+      select('comicsReducer')
+    )
+
+    this.comicsState$.subscribe(state => console.log('state---', state))
+    this.store.dispatch(add({payload: this.comicsService.getAll()})) 
+
+    this.comicsService.getAll().subscribe(res => {
+      this.comics$ = res
+      this.store.dispatch(add({payload: res}))
+    });
+
+  }
+
+  goToDetails(id: string) {
+  }
+
+}
